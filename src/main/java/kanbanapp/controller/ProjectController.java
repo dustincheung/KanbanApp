4093736f9813a0b577d0c7a8e1dd5fd077933f59
Dustin Cheung <dustincheung@Dustins-MBP.fios-router.home>
2020-07-06 14:38:23 -0400

    removed unused imports

--- src/main/java/kanbanapp/controller/ProjectController.java
+++ src/main/java/kanbanapp/controller/ProjectController.java
@@ -1,15 +1,11 @@
 package kanbanapp.controller;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
--- src/main/java/kanbanapp/service/ProjectService.java
+++ src/main/java/kanbanapp/service/ProjectService.java
@@ -1,8 +1,5 @@
 package kanbanapp.service;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
