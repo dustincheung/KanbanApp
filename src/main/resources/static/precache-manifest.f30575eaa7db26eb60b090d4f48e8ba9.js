84c070ccebbd618cbe0870f1e0e66042ca493d51
Dustin Cheung <dustincheung@Dustins-MBP.fios-router.home>
2020-07-28 22:48:49 -0400

    changed react proxy to production backend, built react client and moved static artifacts to static folder of spring backend

---
+++ src/main/resources/static/precache-manifest.f30575eaa7db26eb60b090d4f48e8ba9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34ba4cc82d78155fa3398aba263cf0d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "506edae0645c799e6c06",
+    "url": "/static/js/2.db10a3d5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.db10a3d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e13cb0c0f2e66873bb24",
+    "url": "/static/js/main.3171355b.chunk.js"
+  },
+  {
+    "revision": "1e4886651ffbb6f01ba9",
+    "url": "/static/js/runtime-main.f6b625bf.js"
+  }
+]);
